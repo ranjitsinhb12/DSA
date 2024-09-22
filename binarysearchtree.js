f879a502e42ba28cc565293715bed269b20daefc
ranjitsinhb12 <139944444+ranjitsinhb12@users.noreply.github.com>
2024-09-22 15:14:09 +0000

    add insert in BinarySearchTree

--- binarysearchtree.js
+++ binarysearchtree.js
@@ -10,4 +10,33 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  // insert
+  insert(key){
+    const newNode = new BSTNode(key)
+    if(this.root === null){
+      this.root = newNode
+    }else{
+      this.insertNode(this.root, newNode)
+    }
+  }
+
+  insertNode(node, newNode){ // node is root node
+    if(newNode.key < node.key){
+      if(node.left === null){
+        node.left = newNode
+      }else{
+        this.insertNode(node.left, newNode)
+      }
+    }else{
+      if(node.right === null){
+        node.right = newNode
+      }else{
+        this.insertNode(node.right, newNode)
+      }
+    }
+  }
+
+  // delete
+  
 }
+
