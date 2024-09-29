335561c27fdff7d368d913eab67eec28f857ac16
ranjitsinhb12 <139944444+ranjitsinhb12@users.noreply.github.com>
2024-09-29 14:23:29 +0000

    Add binarysearchtree delete and inOrder preOrder and postOrder Traversal

--- binarysearchtree.js
+++ binarysearchtree.js
@@ -37,6 +37,85 @@ class BinarySearchTree {
   }
 
   // delete
-  
+
+  delete(key){
+    this.root = this.deleteNode(this.root, key)
+  }
+
+  deleteNode(node, key){
+    if(node == null){
+      return null
+    }
+
+    if(key < node.key){
+      node.left = this.deleteNode(node.left, key)
+    }else if(key > node.key){
+      node.right = this.deleteNode(node.right, key)
+    }else{
+      if(node.left === null && node.right === null){
+        return null
+      }else if(node.left == null){
+       return node.right
+      }else if(node.right == null){
+        return node.left
+      }else{
+        let tempNode = findMinNode(node.right)
+        node.key = tempNode.key
+        node.right = this.deleteNode(node.right, tempNode.key)
+
+      }
+    }
+    return node
+  }
+
+  findMinNode(node){
+    while(node !== null){
+      node = node.left
+    }
+    return node
+  }
+  // in order traversal
+  inOrderTraversal(){
+    const result = []
+    this.inOrder(this.root, result)
+    return result
+  }
+
+  inOrder(node, result){
+    if(node !== null){
+      this.inOrder(node.left, result)
+      result.push(node.key)
+      this.inOrder(node.right, result)
+    }
+  }
+  // pre order traversal
+  preOrderTraversal(){
+    const result = []
+    this.preOrder(this.root, result)
+    return result
+  }
+
+  preOrder(node, result){
+    if(node !== null){
+      result.push(node.key)
+      this.preOrder(node.left, result)
+      this.preOrder(node.right, result)
+    }
+  }
+
+  // post order traversal
+  postOrderTraversal(){
+    const result = []
+    this.postOrder(this.root, result)
+    return result
+  }
+
+  postOrder(node, result){
+    if(node !== null){
+      this.postOrder(node.left, result)
+      this.postOrder(node.right, result)
+      result.push(node.key)
+    }
+  }
 }
 
